5d39e29a308134e4186eb8029225a09556c3adae
yoseplee <joseph38.local@gmail.com>
2019-07-20 22:14:08 +0900

    add docker related files and configurations. now can create a rmi server container, but still cannot interacting with host. add configurations using docker-compose to generate multi containers at once will be followed

--- playground/rmi/Client.java
+++ playground/rmi/Client.java
@@ -10,8 +10,10 @@ public class Client {
     public static void main(String[] args) {
 
         String host = (args.length < 1) ? null : args[0];
+        // int port = (args.length < 1) ? null : Integer.parseInt(args[1]);
         try {
-            System.out.println("HOST:: " + host);
+            // System.out.println("HOST:: " + host + "port:: " + port);
+            // Registry registry = LocateRegistry.getRegistry(host);
             Registry registry = LocateRegistry.getRegistry(host);
             Hello stub = (Hello) registry.lookup("Hello");
             // Hello stub = (Hello) registry.lookup("Hello1");
