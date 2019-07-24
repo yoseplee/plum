2f0350e3291ff25f2ca98b325ae8ec3017c9d64b
yoseplee <joseph38.local@gmail.com>
2019-07-24 19:57:51 +0900

    updated client command line interface

---
+++ scripts/client_start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp ../bin/ peer.rmi.Client
--- src/peer/rmi/Client.java
+++ src/peer/rmi/Client.java
@@ -62,17 +62,17 @@ public class Client {
         Scanner scan = new Scanner(System.in);
         ArrayList<String> addressBook = new ArrayList<String>();
         System.out.println("===================");
-        System.out.println("WELCOME TO PEER");
+        System.out.println("WELCOME TO PLUM PEER!");
         System.out.println("===================");
         System.out.println("help command for listing up commands that client have");
         while(true) {
-            System.out.print("> ");
+            System.out.print("&> ");
             String command = "";
             command = scan.nextLine();
             if(command.equals("help")) {
                 help();
             } else if(command.equals("connect")) {
-                System.out.print("Which host(v4)? > ");
+                System.out.print("Which host(v4)? &> ");
                 String host = scan.nextLine();
                 System.out.println("try to connect to host: " + host);
                 client = new Client(host);
@@ -84,6 +84,7 @@ public class Client {
                     stubFromClient.sendMessage("Hello peer!");
                     System.out.println("response: " + response);
                 } catch (Exception e) {
+                    System.err.println("Error! Did you connect to a peer?");
                     System.err.println("Client exception: " + e.toString());
                     e.printStackTrace();
                 }
@@ -93,13 +94,14 @@ public class Client {
                     System.out.println("response: " + response);
                     addressBook.add(response);
                 } catch (Exception e) {
+                    System.err.println("Error! Did you connect to a peer?");
                     System.err.println("Client exception: " + e.toString());
                     e.printStackTrace();
                 }
             } else if(command.equals("address")) {
                 try {    
                     String address = "";
-                    System.out.println("which address to add? > ");
+                    System.out.println("which address to add? &> ");
                     address = scan.nextLine();
                     String response = stubFromClient.addAddress(address);
                     System.out.println("response: " + response);
@@ -118,10 +120,11 @@ public class Client {
                 }
             } else if(command.equals("print addressbook")) {
                 try {    
-                    System.out.println("addressbook of connected peer? > ");
+                    System.out.println("addressbook of connected peer? &> ");
                     String response = stubFromClient.printAllAddressBook();
                     System.out.println("response: " + response);
                 } catch (Exception e) {
+                    System.err.println("Error! Did you connect to a peer?");
                     System.err.println("Client exception: " + e.toString());
                     e.printStackTrace();
                 }
