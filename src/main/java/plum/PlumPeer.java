45bd1cde97cbe204f6ba8d385411508bc3b93ac3
yoseplee <joseph38.local@gmail.com>
2019-08-07 10:55:55 +0900

    update constructor of PlumPeer so that have flexibility on various host and port

--- src/main/java/plum/PlumPeer.java
+++ src/main/java/plum/PlumPeer.java
@@ -5,16 +5,34 @@ import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.util.logging.Logger;
+import java.util.ArrayList;
 
 public class PlumPeer {
-
 	private static final Logger logger = Logger.getLogger(PlumPeer.class.getName());
-
+	private ArrayList<String> addressBook;
 	private Server server;
+	private int port;
+	private String host;
+
+	public PlumPeer() {
+		this("localhost", 50051);
+	}
+
+	public PlumPeer(int port) {
+		this("localhost", port);
+	}
+
+	public PlumPeer(String host, int port) {
+		this.host = host;
+		this.port = port;
+		addressBook = new ArrayList<String>();
+		server = ServerBuilder.forPort(port).addService(new PlumServiceImpl(this)).build();
+	}
 
 	private void start() throws IOException {
-		int port = 50051;
-		server = ServerBuilder.forPort(port).addService(new PlumServiceImpl(this)).build().start();
+		// int port = 50051;
+		// server = ServerBuilder.forPort(port).addService(new PlumServiceImpl(this)).build().start();
+		server.start();
 		logger.info("Server started, listening on " + port);
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 			System.err.println("*** shutting down gRPC server since JVM is shutting down");
